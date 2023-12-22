9d8643bfe1afdcc401dfff2f5a4190f7f88708ca
Mamin-simpotiaga <oliferuk21@gmail.com~>
2023-12-22 20:54:53 +0100

    Dodanie UpdatedOn, CleatedOn;

--- MyTest.Module/BusinessObjects/Core/BaseClass.cs
+++ MyTest.Module/BusinessObjects/Core/BaseClass.cs
@@ -13,6 +13,9 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using MyTest.Module.BusinessObjects.Core.Interfaces;
+using DevExpress.XtraRichEdit.Model;
+using DevExpress.ExpressApp.Model;
+using MyTest.Module.BusinessObjects.CRM;
 
 namespace MyTest.Module.BusinessObjects.Core
 {
@@ -21,22 +24,77 @@ namespace MyTest.Module.BusinessObjects.Core
     [VisibleInDashboards(false)]
     public abstract class BaseClass : XPObject, IObjectCode
     {
+
         public BaseClass(Session session)
         : base(session)
         {
+        }
+
+        protected override void OnSaving()
+        {
+            base.OnSaving();
+            UpdateBaseClassInfo();
         }
 
         [Browsable(false)]
         public bool IsPersistent => GetType().CustomAttributes.ToList().FirstOrDefault((CustomAttributeData x) => x.AttributeType == typeof(NonPersistentAttribute)) == null;
 
         [VisibleInDetailView(false)]
-        public string ObjectCode => GetSystemCode();//
+        public string ObjectCode => GetSystemCode();
 
         public override void AfterConstruction()
         {
             base.AfterConstruction();
             SystemHelper.TempIntOid--;
             base.Oid = SystemHelper.TempIntOid;
+        }
+
+        private string _UpdatedOn;
+        [ModelDefault("AllowEdit", "False")]
+        [VisibleInDetailView(false)]
+        [VisibleInListView(false)]
+        [VisibleInLookupListView(false)]
+        public string UpdatedOn
+        {
+            get
+            {
+                return _UpdatedOn;
+            }
+            set
+            {
+                SetPropertyValue("UpdatedOn", ref _UpdatedOn, value);
+            }
+        }
+
+        private string _CreatedOn;
+        [ModelDefault("AllowEdit", "False")]
+        [VisibleInDetailView(false)]
+        [VisibleInListView(false)]
+        [VisibleInLookupListView(false)]
+        public string CreatedOn
+        {
+            get
+            {
+                return _CreatedOn;
+            }
+            set
+            {
+                SetPropertyValue("CreatedOn", ref _CreatedOn, value);
+            }
+        }
+
+        private void UpdateBaseClassInfo()
+        {
+            if (_UpdatedOn == null && _CreatedOn == null)
+            {
+                _UpdatedOn = "Zaktualizowano " + DateTime.Now.ToString();
+                _CreatedOn = "Utworzono " + DateTime.Now.ToString();
+            }
+            else
+            {
+                _UpdatedOn = "Zaktualizowano " + DateTime.Now.ToString();
+            }
+
         }
 
         //Generwowanie ObjectCode
